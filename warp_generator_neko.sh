cd24dd7d2e13362906cdd33f0669144f01e3adb7
DikozImpact <22dikoz22@gmail.com>
2025-08-21 23:34:59 +0300

    Update warp_generator_neko.sh

--- warp_generator_neko.sh
+++ warp_generator_neko.sh
@@ -36,7 +36,7 @@ conf=$(cat <<-EOM
 "type": "wireguard",
 "local_address": ["${client_ipv4}/32", "${client_ipv6}/128"],
 "peer_public_key": "${peer_pub}",
-"server": "188.114.97.0",
+"server": "162.159.192.1",
 "server_port": 500
 }
 EOM
